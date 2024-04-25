fc163371ab7a41253dfce0029313d532f6079017
Zeus Caballes <79866-23232127@SERVER2.DOMAIN>
2024-04-25 13:33:03 +0800

    hi

---
+++ Enrollment System/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace Enrollment_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CurriculumCodeTextBox = new System.Windows.Forms.TextBox();
+            this.CourseCodeComboBox = new System.Windows.Forms.ComboBox();
+            this.CategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.OfferingComboBox = new System.Windows.Forms.ComboBox();
+            this.UnitsTextBox = new System.Windows.Forms.TextBox();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.SubjCodeTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.RequisiteTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SubjectDataGridView = new System.Windows.Forms.DataGridView();
+            this.SubjectCodeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DescriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UnitsColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CoPreColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PreRequisiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.CoRequisiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SubjectDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CurriculumCodeTextBox);
+            this.groupBox1.Controls.Add(this.CourseCodeComboBox);
+            this.groupBox1.Controls.Add(this.CategoryComboBox);
+            this.groupBox1.Controls.Add(this.OfferingComboBox);
+            this.groupBox1.Controls.Add(this.UnitsTextBox);
+            this.groupBox1.Controls.Add(this.DescriptionTextBox);
+            this.groupBox1.Controls.Add(this.SubjCodeTextBox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(45, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(502, 224);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // CurriculumCodeTextBox
+            // 
+            this.CurriculumCodeTextBox.Location = new System.Drawing.Point(100, 193);
+            this.CurriculumCodeTextBox.Name = "CurriculumCodeTextBox";
+            this.CurriculumCodeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CurriculumCodeTextBox.TabIndex = 13;
+            // 
+            // CourseCodeComboBox
+            // 
+            this.CourseCodeComboBox.FormattingEnabled = true;
+            this.CourseCodeComboBox.Items.AddRange(new object[] {
+            "BSIT",
+            "BSIS",
+            "BSA",
+            "BSHM",
+            "BSN",
+            "Midwifery",
+            "BSCS",
+            "BSBA",
+            "BSED",
+            "BEED",
+            "BSCRIM",
+            "BSTM",
+            "BSECE",
+            "BSCPE",
+            "LAW",
+            ""});
+            this.CourseCodeComboBox.Location = new System.Drawing.Point(100, 162);
+            this.CourseCodeComboBox.Name = "CourseCodeComboBox";
+            this.CourseCodeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.CourseCodeComboBox.TabIndex = 12;
+            // 
+            // CategoryComboBox
+            // 
+            this.CategoryComboBox.FormattingEnabled = true;
+            this.CategoryComboBox.Items.AddRange(new object[] {
+            "LEC",
+            "LAB"});
+            this.CategoryComboBox.Location = new System.Drawing.Point(100, 135);
+            this.CategoryComboBox.Name = "CategoryComboBox";
+            this.CategoryComboBox.Size = new System.Drawing.Size(121, 21);
+            this.CategoryComboBox.TabIndex = 11;
+            // 
+            // OfferingComboBox
+            // 
+            this.OfferingComboBox.FormattingEnabled = true;
+            this.OfferingComboBox.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.OfferingComboBox.Location = new System.Drawing.Point(100, 108);
+            this.OfferingComboBox.Name = "OfferingComboBox";
+            this.OfferingComboBox.Size = new System.Drawing.Size(121, 21);
+            this.OfferingComboBox.TabIndex = 10;
+            // 
+            // UnitsTextBox
+            // 
+            this.UnitsTextBox.Location = new System.Drawing.Point(100, 79);
+            this.UnitsTextBox.Name = "UnitsTextBox";
+            this.UnitsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UnitsTextBox.TabIndex = 9;
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Location = new System.Drawing.Point(100, 50);
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DescriptionTextBox.TabIndex = 8;
+            // 
+            // SubjCodeTextBox
+            // 
+            this.SubjCodeTextBox.Location = new System.Drawing.Point(100, 20);
+            this.SubjCodeTextBox.Name = "SubjCodeTextBox";
+            this.SubjCodeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SubjCodeTextBox.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Curriculum Code";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Course Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Offering";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Units";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subject Code";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(610, 120);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 28);
+            this.SaveButton.TabIndex = 14;
+            this.SaveButton.Text = "&Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(610, 165);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 25);
+            this.ClearButton.TabIndex = 15;
+            this.ClearButton.Text = "&Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.PreRequisiteRadioButton);
+            this.groupBox2.Controls.Add(this.CoRequisiteRadioButton);
+            this.groupBox2.Controls.Add(this.RequisiteTextBox);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.SubjectDataGridView);
+            this.groupBox2.Location = new System.Drawing.Point(45, 242);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(515, 211);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Requisite Information";
+            // 
+            // RequisiteTextBox
+            // 
+            this.RequisiteTextBox.Location = new System.Drawing.Point(142, 26);
+            this.RequisiteTextBox.Multiline = true;
+            this.RequisiteTextBox.Name = "RequisiteTextBox";
+            this.RequisiteTextBox.Size = new System.Drawing.Size(106, 27);
+            this.RequisiteTextBox.TabIndex = 2;
+            this.RequisiteTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RequisiteTextBox_KeyPress);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(62, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Subject Code:";
+            // 
+            // SubjectDataGridView
+            // 
+            this.SubjectDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SubjectDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SubjectCodeColumn,
+            this.DescriptionColumn,
+            this.UnitsColumn,
+            this.CoPreColumn});
+            this.SubjectDataGridView.Location = new System.Drawing.Point(24, 63);
+            this.SubjectDataGridView.Name = "SubjectDataGridView";
+            this.SubjectDataGridView.RowHeadersVisible = false;
+            this.SubjectDataGridView.Size = new System.Drawing.Size(478, 133);
+            this.SubjectDataGridView.TabIndex = 0;
+            this.SubjectDataGridView.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RequisiteTextBox_KeyPress);
+            // 
+            // SubjectCodeColumn
+            // 
+            this.SubjectCodeColumn.HeaderText = "Subject Code";
+            this.SubjectCodeColumn.Name = "SubjectCodeColumn";
+            this.SubjectCodeColumn.ReadOnly = true;
+            // 
+            // DescriptionColumn
+            // 
+            this.DescriptionColumn.HeaderText = "Subject Description";
+            this.DescriptionColumn.Name = "DescriptionColumn";
+            this.DescriptionColumn.ReadOnly = true;
+            // 
+            // UnitsColumn
+            // 
+            this.UnitsColumn.HeaderText = "Subject Units";
+            this.UnitsColumn.Name = "UnitsColumn";
+            this.UnitsColumn.ReadOnly = true;
+            // 
+            // CoPreColumn
+            // 
+            this.CoPreColumn.HeaderText = "Co/Pre";
+            this.CoPreColumn.Name = "CoPreColumn";
+            this.CoPreColumn.ReadOnly = true;
+            // 
+            // PreRequisiteRadioButton
+            // 
+            this.PreRequisiteRadioButton.AutoSize = true;
+            this.PreRequisiteRadioButton.Location = new System.Drawing.Point(344, 19);
+            this.PreRequisiteRadioButton.Name = "PreRequisiteRadioButton";
+            this.PreRequisiteRadioButton.Size = new System.Drawing.Size(83, 17);
+            this.PreRequisiteRadioButton.TabIndex = 17;
+            this.PreRequisiteRadioButton.TabStop = true;
+            this.PreRequisiteRadioButton.Text = "Pre-requisite";
+            this.PreRequisiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CoRequisiteRadioButton
+            // 
+            this.CoRequisiteRadioButton.AutoSize = true;
+            this.CoRequisiteRadioButton.Location = new System.Drawing.Point(344, 42);
+            this.CoRequisiteRadioButton.Name = "CoRequisiteRadioButton";
+            this.CoRequisiteRadioButton.Size = new System.Drawing.Size(80, 17);
+            this.CoRequisiteRadioButton.TabIndex = 18;
+            this.CoRequisiteRadioButton.TabStop = true;
+            this.CoRequisiteRadioButton.Text = "Co-requisite";
+            this.CoRequisiteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(610, 211);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 25);
+            this.NextButton.TabIndex = 17;
+            this.NextButton.Text = "&Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SubjectDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox CurriculumCodeTextBox;
+        private System.Windows.Forms.ComboBox CourseCodeComboBox;
+        private System.Windows.Forms.ComboBox CategoryComboBox;
+        private System.Windows.Forms.ComboBox OfferingComboBox;
+        private System.Windows.Forms.TextBox UnitsTextBox;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.TextBox SubjCodeTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView SubjectDataGridView;
+        private System.Windows.Forms.TextBox RequisiteTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SubjectCodeColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescriptionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UnitsColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CoPreColumn;
+        private System.Windows.Forms.RadioButton PreRequisiteRadioButton;
+        private System.Windows.Forms.RadioButton CoRequisiteRadioButton;
+        private System.Windows.Forms.Button NextButton;
+    }
+}
+
---
+++ Enrollment System/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Enrollment_System
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubjectEntryButton = new System.Windows.Forms.Button();
+            this.SubjectScheduleEntryButton = new System.Windows.Forms.Button();
+            this.SubjectEnrollmentEntryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SubjectEntryButton
+            // 
+            this.SubjectEntryButton.Location = new System.Drawing.Point(257, 67);
+            this.SubjectEntryButton.Name = "SubjectEntryButton";
+            this.SubjectEntryButton.Size = new System.Drawing.Size(210, 55);
+            this.SubjectEntryButton.TabIndex = 0;
+            this.SubjectEntryButton.Text = "Subject Entry";
+            this.SubjectEntryButton.UseVisualStyleBackColor = true;
+            // 
+            // SubjectScheduleEntryButton
+            // 
+            this.SubjectScheduleEntryButton.Location = new System.Drawing.Point(257, 155);
+            this.SubjectScheduleEntryButton.Name = "SubjectScheduleEntryButton";
+            this.SubjectScheduleEntryButton.Size = new System.Drawing.Size(210, 55);
+            this.SubjectScheduleEntryButton.TabIndex = 1;
+            this.SubjectScheduleEntryButton.Text = "Subject Schedule Entry";
+            this.SubjectScheduleEntryButton.UseVisualStyleBackColor = true;
+            // 
+            // SubjectEnrollmentEntryButton
+            // 
+            this.SubjectEnrollmentEntryButton.Location = new System.Drawing.Point(257, 247);
+            this.SubjectEnrollmentEntryButton.Name = "SubjectEnrollmentEntryButton";
+            this.SubjectEnrollmentEntryButton.Size = new System.Drawing.Size(210, 55);
+            this.SubjectEnrollmentEntryButton.TabIndex = 2;
+            this.SubjectEnrollmentEntryButton.Text = "Subject Enrollment Entry";
+            this.SubjectEnrollmentEntryButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SubjectEnrollmentEntryButton);
+            this.Controls.Add(this.SubjectScheduleEntryButton);
+            this.Controls.Add(this.SubjectEntryButton);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SubjectEntryButton;
+        private System.Windows.Forms.Button SubjectScheduleEntryButton;
+        private System.Windows.Forms.Button SubjectEnrollmentEntryButton;
+    }
+}
